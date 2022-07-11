bf94f8a55095e6bb03677c2e78c848b321f0b871
Nell Fauveau <nellousan@hotmail.fr>
2022-07-11 15:50:58 +0200

    feat(WoL): completed WoL packet maker

--- hsm/wol/wol.go
+++ hsm/wol/wol.go
@@ -1,1 +1,28 @@
 package wol
+
+const pckt_len = 6 + 16*6
+
+type MacAddress [6]byte
+type MagicPacket [pckt_len]byte
+
+func MakeWolPacket(macAddress MacAddress) MagicPacket {
+	var packet MagicPacket
+
+	packet[0] = 0xFF
+	packet[1] = 0xFF
+	packet[2] = 0xFF
+	packet[3] = 0xFF
+	packet[4] = 0xFF
+	packet[5] = 0xFF
+
+	for i := 6; i < pckt_len; i++ {
+		packet[i] = macAddress[i%6]
+	}
+
+	return packet
+}
+
+func BroadcastWolPacket(magicPacket MagicPacket) error {
+
+	return nil
+}
