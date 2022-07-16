74f14dd952627be06c8c20fcd4b503f2c032f05b
Nell Fauveau <nellousan@hotmail.fr>
2022-07-16 13:20:15 +0200

    feat(Wol): Added NewMacAddresFromString() and NewPacketFromString() along with unit test

--- hsm/wol/wol.go
+++ hsm/wol/wol.go
@@ -1,11 +1,43 @@
 package wol
 
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 const pckt_len = 6 + 16*6
 
 type MacAddress [6]byte
 type MagicPacket [pckt_len]byte
 
-func MakeWolPacket(macAddress MacAddress) MagicPacket {
+// Creates a new MacAddress from string.
+// Returns an error in case of failing to parse the string.
+func NewMacAddressFromString(addrStr string) (MacAddress, error) {
+	match, err := regexp.MatchString("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", addrStr)
+	if err != nil {
+		return MacAddress{}, err
+	}
+
+	if match == false {
+		return MacAddress{}, errors.New(fmt.Sprintf("NewMacAddressFromString: addrStr does not match regexp: \"%s\"", addrStr))
+	}
+
+	addrStr = strings.ReplaceAll(addrStr, ":", "")
+	addrStr = strings.ReplaceAll(addrStr, "-", "")
+
+	res, err := hex.DecodeString(addrStr)
+	if err != nil {
+		return MacAddress{}, err
+	}
+
+	return MacAddress{res[0], res[1], res[2], res[3], res[4], res[5]}, nil
+}
+
+// Creates a new Packet from a MacAddress.
+func NewPacket(macAddress MacAddress) MagicPacket {
 	var packet MagicPacket
 
 	packet[0] = 0xFF
@@ -22,7 +54,18 @@ func MakeWolPacket(macAddress MacAddress) MagicPacket {
 	return packet
 }
 
-func BroadcastWolPacket(magicPacket MagicPacket) error {
+// Is equivalent to calling NewMacAddressFromString() and NewPacket() subsequently.
+func NewPacketFromString(addrStr string) (MagicPacket, error) {
+	addr, err := NewMacAddressFromString(addrStr)
+	if err != nil {
+		return MagicPacket{}, err
+	}
+
+	packet := NewPacket(addr)
+	return packet, nil
+}
+
+func BroadcastPacket(magicPacket MagicPacket) error {
 
 	return nil
 }
---
+++ hsm/wol/wol_test.go
@@ -0,0 +1,95 @@
+package wol
+
+import "testing"
+
+func TestNewMacAddressFromString(t *testing.T) {
+	addrStr1 := "01-02-05-FF-FE-55"
+	addrStr2 := "01-02-05-ff-fe-55"
+	addrStr3 := "01:02:05:FF:FE:55"
+	addrStr4 := "01.02.05.FF.FE.55"
+	addrStr5 := "01-02-05-FF-GE-55"
+
+	// addrStr1
+	addr, err := NewMacAddressFromString(addrStr1)
+	if err != nil {
+		t.Errorf("%s \"%s\"", err.Error(), addrStr1)
+	}
+
+	if !(addr[0] == 0x01 && addr[1] == 0x02 && addr[2] == 0x05 && addr[3] == 0xFF && addr[4] == 0xFE && addr[5] == 0x55) {
+		t.Errorf("Unexpected values: \"%s\"", addrStr1)
+	}
+
+	// addrStr2
+	addr, err = NewMacAddressFromString(addrStr2)
+	if err != nil {
+		t.Errorf("%s \"%s\"", err.Error(), addrStr2)
+	}
+
+	if !(addr[0] == 0x01 && addr[1] == 0x02 && addr[2] == 0x05 && addr[3] == 0xFF && addr[4] == 0xFE && addr[5] == 0x55) {
+		t.Errorf("Unexpected values: \"%s\"", addrStr2)
+	}
+
+	// addrStr3
+	addr, err = NewMacAddressFromString(addrStr3)
+	if err != nil {
+		t.Errorf("%s \"%s\"", err.Error(), addrStr3)
+	}
+
+	if !(addr[0] == 0x01 && addr[1] == 0x02 && addr[2] == 0x05 && addr[3] == 0xFF && addr[4] == 0xFE && addr[5] == 0x55) {
+		t.Errorf("Unexpected values: \"%s\"", addrStr3)
+	}
+
+	// addrStr4
+	addr, err = NewMacAddressFromString(addrStr4)
+	if err == nil {
+		t.Errorf("Should return an error: \"%s\"", addrStr4)
+	}
+
+	// addrStr5
+	addr, err = NewMacAddressFromString(addrStr5)
+	if err == nil {
+		t.Errorf("Should return an error: \"%s\"", addrStr5)
+	}
+}
+
+func TestNewPacket(t *testing.T) {
+	address := MacAddress{0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	packet := NewPacket(address)
+
+	for i := 0; i < 6; i++ {
+		if !(packet[i] == 0xFF) {
+			t.Errorf("Malformed packet. {i = %d}", i)
+			break
+		}
+	}
+
+	for i := 6; i < len(packet); i++ {
+		if !(packet[i] == address[i%6]) {
+			t.Errorf("Malformed packet. {i = %d}", i)
+			break
+		}
+	}
+}
+
+func TestNewPacketFromString(t *testing.T) {
+	addressCheck := MacAddress{0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	address := "01:02:03:04:05:06"
+	packet, err := NewPacketFromString(address)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	for i := 0; i < 6; i++ {
+		if !(packet[i] == 0xFF) {
+			t.Errorf("Malformed packet. {i = %d}", i)
+			break
+		}
+	}
+
+	for i := 6; i < len(packet); i++ {
+		if !(packet[i] == addressCheck[i%6]) {
+			t.Errorf("Malformed packet. {i = %d}", i)
+			break
+		}
+	}
+}
