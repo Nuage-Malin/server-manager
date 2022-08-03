3ef603683f5be158a9a1636e3d572899e3581ca5
Nell Fauveau <nellousan@hotmail.fr>
2022-08-03 15:47:49 +0200

    feat(conf): added port and username to conf, also added a sanity check of the ConfFile

--- hsm/conf/conf.go
+++ hsm/conf/conf.go
@@ -7,16 +7,41 @@ import (
 )
 
 type ServerUnit struct {
-	Name       string `json:"name"`
-	SshKey     string `json:"ssh_key"`
-	IpAddress  string `json:"ip_address"`
-	MacAddress string `json:"mac_address"`
+	Name        string `json:"name"`
+	SshUsername string `json:"ssh_username"`
+	SshKey      string `json:"ssh_key"`
+	IpAddress   string `json:"ip_address"`
+	SshPort     int    `json:"ssh_port"`
+	MacAddress  string `json:"mac_address"`
 }
 
 type ConfFile struct {
 	Servers []ServerUnit `json:"servers"`
 }
 
+func contains(s []string, f string) bool {
+	for _, elem := range s {
+		if elem == f {
+			return true
+		}
+	}
+	return false
+}
+
+// Check the sanity of the ConfFile, panic if an error is present
+func SanityCheck(conf *ConfFile) {
+	checked := make([]string, 0, len(conf.Servers))
+
+	for _, serv := range conf.Servers {
+		if contains(checked, serv.Name) {
+			panic("2 servers have the same name.")
+		}
+		checked = append(checked, serv.Name)
+	}
+}
+
+// Find Server by name.
+// Returns the corresponding ServerUnit
 func (c *ConfFile) FindServerUnitByName(name string) (*ServerUnit, error) {
 	for _, server := range c.Servers {
 		if server.Name == name {
